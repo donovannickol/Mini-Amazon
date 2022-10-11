128afacf4a182935a1c025b3aff66d59409f743b
Akash Mullick <am856@duke.edu>
2022-10-11 19:58:40 -0400

    Sample product and category db code

---
+++ db/load_ms2.sql
@@ -0,0 +1,21 @@
+\COPY Users FROM 'Users.csv' WITH DELIMITER ',' NULL '' CSV
+-- since id is auto-generated; we need the next command to adjust the counter
+-- for auto-generation so next INSERT will not clash with ids loaded above:
+SELECT pg_catalog.setval('public.users_id_seq',
+                         (SELECT MAX(id)+1 FROM Users),
+                         false);
+
+\COPY Products FROM 'Products.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.products_id_seq',
+                         (SELECT MAX(id)+1 FROM Products),
+                         false);
+
+\COPY Categories FROM 'Categories.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.categories_id_seq',
+                         (SELECT MAX(id)+1 FROM Categories),
+                         false);       
+
+\COPY Purchases FROM 'Purchases.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.purchases_id_seq',
+                         (SELECT MAX(id)+1 FROM Purchases),
+                         false);
