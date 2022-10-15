033516e12b3c505c79a1114f26b8f17d086b24bb
Akash Mullick <am856@duke.edu>
2022-10-14 22:55:06 -0400

    Use generated products and make table searchable + paginated

--- db/generated/gen.py
+++ db/generated/gen.py
@@ -28,7 +28,11 @@ def gen_users(num_users):
             name_components = profile['name'].split(' ')
             firstname = name_components[0]
             lastname = name_components[-1]
-            writer.writerow([uid, email, password, firstname, lastname])
+            address = profile['address']
+            city = profile['current_location'][0]
+            state = profile['current_location'][1]
+            balance = f'{str(fake.random_int(max=500))}.{fake.random_int(max=99):02}'
+            writer.writerow([uid, email, firstname, lastname, address, city, state, balance])
         print(f'{num_users} generated')
     return
 
@@ -41,14 +45,27 @@ def gen_products(num_products):
         for pid in range(num_products):
             if pid % 100 == 0:
                 print(f'{pid}', end=' ', flush=True)
-            name = fake.sentence(nb_words=4)[:-1]
+            name = fake.sentence(nb_words=2)[:-1]
+            description = fake.paragraph(nb_sentences=4)
+            # generate random valid img url
+            # img_url = f'https://picsum.photos/id/{fake.random_int(min=0, max=500)}/200'
+            # generate img url using faker
+            img_url = fake.image_url(width=200, height=200)
             price = f'{str(fake.random_int(max=500))}.{fake.random_int(max=99):02}'
-            available = fake.random_element(elements=('true', 'false'))
-            if available == 'true':
-                available_pids.append(pid)
-            writer.writerow([pid, name, price, available])
+            category = fake.random_element(elements=['Books', 'Movies', 'Music'])
+            available_pids.append(pid)
+            writer.writerow([pid, name, description, img_url, price, category])
         print(f'{num_products} generated; {len(available_pids)} available')
     return available_pids
+
+def gen_categories():
+    with open('Categories.csv', 'w') as f:
+        writer = get_csv_writer(f)
+        print('Categories...', end=' ', flush=True)
+        for category in ['Books', 'Movies', 'Music']:
+            writer.writerow([category])
+        print('generated')
+    return
 
 
 def gen_purchases(num_purchases, available_pids):
@@ -67,5 +84,6 @@ def gen_purchases(num_purchases, available_pids):
 
 
 gen_users(num_users)
+gen_categories()
 available_pids = gen_products(num_products)
 gen_purchases(num_purchases, available_pids)
