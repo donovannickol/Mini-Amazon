ad8a6cd5f942f8c36fb3b7a44e74511457cb887d
am856 <am856@duke.edu>
2022-10-16 22:10:26 -0400

    Add search features to card view

--- app/models/product.py
+++ app/models/product.py
@@ -42,21 +42,25 @@ LIMIT :k
         return [Product(*row) for row in rows]
 
     @staticmethod
-    def get_page_of_products(page, limit=8):
+    def get_page_of_products(page, limit=8, search_term=""):
         rows = app.db.execute('''
 SELECT id, name, description, img_url, price, category
 FROM Products
+WHERE name LIKE :search_term OR description LIKE :search_term
 LIMIT :limit
 OFFSET :page
 ''',
+                              search_term='%' + search_term + '%',
                               page=page*limit,
                               limit=limit)
         return [Product(*row) for row in rows]
 
     @staticmethod
-    def get_num_avail_products():
+    def get_num_matching_products(search_term=""):
         rows = app.db.execute('''
 SELECT COUNT(*)
 FROM Products
-''')
+WHERE name LIKE :search_term OR description LIKE :search_term
+''',
+                              search_term='%' + search_term + '%')
         return rows[0][0]
