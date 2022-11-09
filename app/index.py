90dd806faa9bb3a833b52a3f463d8ffc07e6e514
Joshua Kang <jk452@duke.edu>
2022-11-08 22:31:17 -0500

    Merged the edits into oldIndex

--- app/index.py
+++ app/index.py
@@ -19,15 +19,8 @@ def index():
     search_term = request.args.get('search_term', "", type=str)
     sort_by=request.args.get('sort_by', "Default", type=str)
     num_products = Product.get_num_matching_products(search_term, category)
-    products = Product.get_page_of_products(page, PRODUCTS_PER_PAGE, search_term, sort_by, category)
-
-    #productRatings if they're signed in
-    #productRating = productRating.get_by_user_id_tot(user_id)
-    if current_user.is_authenticated:
-        productRatings = ProductRating.get_by_user_id_tot(1) #change to current_user.id
-    else:
-        productRatings = None
-    return render_template('index.html', avail_products=products, num_products=num_products, products_per_page = PRODUCTS_PER_PAGE, curr_page = page, search_term = search_term, sort_by = sort_by, categories = categories, curr_category = category, productRatings=productRatings)
+    products = Product.get_page_of_products(page, PRODUCTS_PER_PAGE, search_term, sort_by, category)    
+    return render_template('index.html', avail_products=products, num_products=num_products, products_per_page = PRODUCTS_PER_PAGE, curr_page = page, search_term = search_term, sort_by = sort_by, categories = categories, curr_category = category)
 
 
 # route to our old home page (up until 10/27/2022)
@@ -41,7 +34,16 @@ def oldIndex():
             current_user.id, datetime.datetime(1980, 9, 14, 0, 0, 0))
     else:
         purchases = None
+
+    #productRatings if they're signed in
+    #productRating = productRating.get_by_user_id_tot(user_id)
+    if current_user.is_authenticated:
+        productRatings = ProductRating.get_by_user_id_tot(1) #change to current_user.id
+    else:
+        productRatings = None
+
     # render the page by adding information to the index.html file
     return render_template('old_index.html',
                            avail_products=products,
-                           purchase_history=purchases)
+                           purchase_history=purchases,
+                           productRatings=productRatings)
